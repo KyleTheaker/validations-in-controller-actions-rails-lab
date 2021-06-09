6175100402dc9f3fa7618f9c73037702c74f889e
Kyle Theaker <kyle.theaker@yahoo.com>
2021-06-09 15:31:39 -0700

    commit tree 5a21757fbe806f1236e558938ba4db8b160a3b20

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+    a = ["Fiction", "Non-Fiction"]
+    validates :title, presence: true
+    validates :category, inclusion: {in: a }
+    validates :content, length: {minimum:100}
 end
